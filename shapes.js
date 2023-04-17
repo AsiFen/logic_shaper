92d7905b3ecafbee6f0091f6f8d60bf65394b41b
Asisipho <missfente@gmail.com>
2023-04-17 16:13:51 +0200

    initial

--- shapes.js
+++ shapes.js
@@ -1,36 +1,37 @@
 //	Which shape’s sum of numbers is the lowest? 
 //Create a function called lowestSumForShape that can find out.
 
-function lowestSumForShape(shapes) {
+function lowestSumForShape(shapesArray) {
   var holder = {}
-  var shapez = ''
+  var shapes = ''
   var min = Infinity;
 
 
-  if (shapes !== null && shapes !== '') {
-    if (typeof shapes == 'object') {
+  if (shapesArray !== null && shapesArray !== '') {
+    if (typeof shapesArray == 'object') {
+      var getShapes = function(shape) {
+        for (var i = 0; i < shapesArray.length; i++) {
+          var type = shapesArray[i].type
+          if (holder[type] === undefined) {
+            holder[type] = shapesArray[i].number
 
-      for (var i = 0; i < shapes.length; i++) {
-        var type = shapes[i].type
-        if (holder[type] === undefined) {
-          holder[type] = shapes[i].number
+          }
 
-        }
-
-        else {
-          holder[type] += shapes[i].number
-        }
-
-      }
-      for (var x in holder) {
-        if (holder[x] < min) {
-          min = holder[x]
-          shapez = x
-
+          else {
+            holder[type] += shapesArray[i].number
+          }
         }
       }
+      var getLowest= function() {
+        for (var x in holder) {
+          if (holder[x] < min) {
+            min = holder[x]
+            shapes = x
 
-      return shapez
+          }
+        }
+      return shapes
+    }
     }
 
     else {
@@ -39,5 +40,10 @@ function lowestSumForShape(shapes) {
   }
   else {
     return 'please enter valid data'
+  }
+
+  return {
+    getLowest,
+    getShapes
   }
 }
--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,11 +1,15 @@
 describe('lowestSumForShape function ', function () {
     it('return the shape that has the lowest sum from given data set', function () {
+        var shapesArray = [{"type":"triangle","color":"green","number":7,"size":"small"},
+        {"type":"circle","color":"orange","number":1,"size":"small"}]
+      
+        var lowestSumShape = lowestSumForShape(shapesArray);
+        lowestSumShape.getShapes(shapesArray)
 
-     var shapes = [{"type":"triangle","color":"green","number":7,"size":"small"},
-      {"type":"circle","color":"orange","number":1,"size":"small"}]
-      assert.equal('circle', lowestSumForShape(shapes));
+      assert.equal('circle', lowestSumShape.getLowest());
 
     });
+    
     it ("should return please enter valid data", function(){
         assert.equal('please enter valid data', lowestSumForShape(''));
 
@@ -13,5 +17,9 @@ describe('lowestSumForShape function ', function () {
     it("should return please enter valid input when the",function(){
 
         assert.equal('no input entered',lowestSumForShape("saffah"))
+    })
+    it('gets all the shapes availables and their summed numbers', function(){
+       
+
     })
 });
